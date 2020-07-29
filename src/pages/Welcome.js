6675e0710be47486a8d5b2578aa850c7e3e50be6
haleykelling <haley@kellings.com>
2020-07-29 17:44:15 -0600

    Fixed bug.

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,7 +7,7 @@ const Welcome = () => {
     return (
         <div>
             <h1 className="app-name">Right Track</h1>
-            <h2 className="description-of-app">Your personal habit tracker</h2>
+            <h2 className="description-of-app">your personal habit tracker</h2>
             <div className="quote-block">
                 <p className="quote">“ We are what we repeatedly do. Excellence, then, is not an act, but a habit. ”</p>
                 <p className="aristotle">-Aristotle</p>
