229e6d003d7b38375a3cda1ca6add414510036f8
haleykelling <haley@kellings.com>
2020-07-30 18:19:43 -0600

    Full functionality for editing a habit created with pessimistic rendering.

--- src/components/HabitItem.js
+++ src/components/HabitItem.js
@@ -7,6 +7,7 @@ import '../Dashboard.css';
 const HabitItem = ({habit, deleteHabit, editHabit, markCompletedHabit}) => {
     
     const [edit, setEdit] = useState(false)
+    const [input, setInput] = useState({})
 
     const toggleEdit = () => setEdit(!edit)
 
@@ -17,6 +18,22 @@ const HabitItem = ({habit, deleteHabit, editHabit, markCompletedHabit}) => {
 
     const handleEditClick = (event) => {
         event.preventDefault()
+        toggleEdit()
+    }
+
+    const handleInput = (event) => {
+        setInput({
+            ...input,
+            [event.target.name]: event.target.value 
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setInput({})
+        editHabit(habit.id, input)
+        event.target.reset()
+        toggleEdit()
     }
 
     const handleCompletedClick = (event) => {
@@ -27,14 +44,22 @@ const HabitItem = ({habit, deleteHabit, editHabit, markCompletedHabit}) => {
     return (
         <div className="habit-card">
             {edit
-                ? <form>
-                    <input name="name">{habit.name}</input>
-                    <input name="details">{habit.details}</input>
+                ? <form className="edit-form" onSubmit={handleSubmit}>
+                    <input name="name" placeholder={habit.name} onChange={handleInput}/>
+                    <textarea 
+                        name="details" 
+                        rows="4"
+                        placeholder={habit.details} 
+                        onChange={handleInput}
+                    />
+                    <input type="submit" value="Edit"/>
                 </form>
-                : <h3>{habit.name}</h3>
-                <p>{habit.details}</p>
-                <p>Current Streak: {habit.current_streak}</p>
-                <p>Longest Streak: {habit.longest_streak}</p>
+                : <>
+                    <h3>{habit.name}</h3>
+                    <p>{habit.details}</p>
+                    <p>Current Streak: {habit.current_streak}</p>
+                    <p>Longest Streak: {habit.longest_streak}</p>
+                </>
             }
             <span className="habit-card-buttons">
                 <button onClick={handleCompletedClick}>
